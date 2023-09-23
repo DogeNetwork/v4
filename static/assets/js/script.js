4d1901d6754e87eac800960309577ed4c5c0cf6c
Derpman <immapotato8110@gmail.com>
2023-09-22 23:02:08 -0400

    a

--- static/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
