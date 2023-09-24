adf34afefc2dee1e14ef22d449bd845a848e1b53
Derpman <immapotato8110@gmail.com>
2023-09-23 23:14:54 -0400

    l

--- static/assets/js/mobile.js
+++
@@ -1,8 +0,0 @@
-let details = navigator.userAgent;
-let regexp = /android|iphone|kindle|ipad/i;
-let isMobileDevice = regexp.test(details);
-if (isMobileDevice) {
- window.location.href = '/mobile.html';
-} else {
-    console.log('I love big tall guys twerking in the shower');
-}
