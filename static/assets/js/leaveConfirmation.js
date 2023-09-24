c23bec776b2e4fc837c7a7378f473f9cf39887b7
Derpman <immapotato8110@gmail.com>
2023-09-24 00:04:52 -0400

    asd

--- static/assets/js/leaveConfirmation.js
+++
@@ -1,6 +0,0 @@
-/*
-@Russell2259 removed this cause it was annoying. we can add it as a setting
-
-window.onbeforeunload = function(e) {  return "Do you want to exit this page?";};
-console.log("leaveConfirmation.js activated.");
-*/
