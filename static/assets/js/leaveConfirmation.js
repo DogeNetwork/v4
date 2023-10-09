f06c94ef626380b7a1b5fb290f286a1f7d37a187
Derpman <immapotato8110@gmail.com>
2023-10-08 21:03:19 -0400

    a

---
+++ static/assets/js/leaveConfirmation.js
@@ -0,0 +1,9 @@
+/*
+@Russell2259 removed this cause it was annoying. we can add it as a setting
+
+9/24/2023 added as a setting by Derpman^^
+9/25/2023 removed since it was buggy by derpman
+
+window.onbeforeunload = function(e) {  return "Do you want to exit this page?";};
+console.log("leaveConfirmation.js activated.");
+*/
