dcf43d90c4ddb5394782fd20f50512bbb91df19d
Derpman <immapotato8110@gmail.com>
2023-11-10 20:35:03 -0500

    update por

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ server.on("upgrade", (req, socket, head) => {
 });
 
 server.on("listening", () => {
-  console.log(`Doge Unblocker running at port 8080`);
+  console.log(`Doge Unblocker running at port 8000`);
 });
 
 server.listen({
