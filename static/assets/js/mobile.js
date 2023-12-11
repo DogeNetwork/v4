fc91902910feb19d8dc5ec36f5ec08c90c5cc21c
Derpman <immapotato8110@gmail.com>
2023-12-10 21:15:05 -0500

    a

--- static/assets/js/mobile.js
+++
@@ -1,8 +0,0 @@
-let details = navigator.userAgent;
-let regexp = /android|iphone|kindle|ipad/i;
-let isMobileDevice = regexp.test(details);
-if (isMobileDevice) {
- window.location.href = '/mobile.html';
-} else {
-    console.log('Desktop Detected.');
-}
