718e5544cc45705ff7254312b91e772d43131abf
fomer3996 <156138999+fomer3996@users.noreply.github.com>
2024-01-09 17:34:25 -0600

    Update ag.js

--- static/assets/js/ag.js
+++ static/assets/js/ag.js
@@ -176,3 +176,128 @@ function paperIo() {
   sessionStorage.setItem('agUrl', agU);
   location.href = '/lessons';
 }
+function bitlife() {
+  agU = Ultraviolet.codec.xor.encode('https://ubg365.github.io/bitlife-life-simulator/play.html');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function buildroyaleio() {
+  agU = Ultraviolet.codec.xor.encode('https://buildroyale.io/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function yohohoio() {
+  agU = Ultraviolet.codec.xor.encode('https://yohoho.io/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function btd4() {
+  agU = Ultraviolet.codec.xor.encode('https://cdn2.addictinggames.com/addictinggames-content/ag-assets/content-items/html5-games/bloonstowerdefense4/index.html');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function ducklife1() {
+  agU = Ultraviolet.codec.xor.encode('https://games.wixgames.co.uk/games/ducklife1/webgl/?timestamp=1704841057&countryCode=en&siteLocale=en&locale=en&gd_sdk_referrer_url=https%3A%2F%2Fkizi.com%2Fgames%2Fduck-life/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function ducklife2() {
+  agU = Ultraviolet.codec.xor.encode('https://games.wixgames.co.uk/games/ducklife2/webgl/?timestamp=1704841115&countryCode=en&siteLocale=en&locale=en&gd_sdk_referrer_url=https%3A%2F%2Fkizi.com%2Fgames%2Fduck-life-2');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function ducklife3() {
+  agU = Ultraviolet.codec.xor.encode('https://games.wixgames.co.uk/games/ducklife3/webgl/?timestamp=1704841136&countryCode=en&siteLocale=en&locale=en&gd_sdk_referrer_url=https%3A%2F%2Fkizi.com%2Fgames%2Fduck-life-3');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function crossyroad() {
+  agU = Ultraviolet.codec.xor.encode('https://i.gamesgo.net/uploads/game/html5/4072/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function ducklife4() {
+  agU = Ultraviolet.codec.xor.encode('https://www.mathplayground.com/duck4/index.html?timestamp=1704841157&countryCode=en&siteLocale=en&locale=en&gd_sdk_referrer_url=https%3A%2F%2Fkizi.com%2Fgames%2Fducklife-4');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function drifthunters() {
+  agU = Ultraviolet.codec.xor.encode('https://unblockedgamesroom.com/wp-content/uploads/assets/drift-hunters/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function fnaf() {
+  agU = Ultraviolet.codec.xor.encode('https://run3.io/popgame/fnaf/fnaf1/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function jetpackjoyride() {
+  agU = Ultraviolet.codec.xor.encode('https://ext.minijuegosgratis.com/jetpack-joyride/index.html?mp_api_as3_url=https%3A%2F%2Fssl.minijuegosgratis.com%2Flechuck%2Fas3%2Flatest.swf&mp_api_as3_url_bck=https%3A%2F%2Fapi.minijuegos.com%2Flechuck%2Fclient-as%2F&mp_api_id=4100&mp_api_js_url=https%3A%2F%2Fssl.minijuegosgratis.com%2Flechuck%2Fjs%2Flatest.js&mp_api_js_url_bck=https%3A%2F%2Fapi.minijuegos.com%2Flechuck%2Fclient-js%2F&mp_assets=https%3A%2F%2Fs2.minijuegosgratis.com%2F&mp_embed=0&mp_game_id=224491&mp_game_uid=jetpack-joyride&mp_game_url=https%3A%2F%2Fwww.miniplay.com%2Fembed%2Fjetpack-joyride&mp_int=1&mp_locale=en_US&mp_player_type=IFRAME&mp_site_https_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_site_name=miniplay.com&mp_site_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_timezone=America%2FChicago&mp_view_type=&mini_signature=5cf17550a654849f15716dc389b6c6ea&xdm_e=https%3A%2F%2Fwww.miniplay.com&xdm_c=default8394&xdm_p=1');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function impossiblequiz() {
+  agU = Ultraviolet.codec.xor.encode('https://cdn2.addictinggames.com/addictinggames-content/ag-assets/content-items/html5-games/theimpossiblequiz/index.html');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function motox3m() {
+  agU = Ultraviolet.codec.xor.encode('https://html5.gamedistribution.com/rvvASMiM/5b0abd4c0faa4f5eb190a9a16d5a1b4c/index.html?gd_sdk_referrer_url=https%3A%2F%2Fwww.miniplay.com%2Fgame%2Fmoto-x3m&mp_api_as3_url=https%3A%2F%2Fssl.minijuegosgratis.com%2Flechuck%2Fas3%2Flatest.swf&mp_api_as3_url_bck=https%3A%2F%2Fapi.minijuegos.com%2Flechuck%2Fclient-as%2F&mp_api_id=3568&mp_api_js_url=https%3A%2F%2Fssl.minijuegosgratis.com%2Flechuck%2Fjs%2Flatest.js&mp_api_js_url_bck=https%3A%2F%2Fapi.minijuegos.com%2Flechuck%2Fclient-js%2F&mp_assets=https%3A%2F%2Fs2.minijuegosgratis.com%2F&mp_embed=0&mp_game_id=209046&mp_game_uid=moto-x3m&mp_game_url=https%3A%2F%2Fwww.miniplay.com%2Fembed%2Fmoto-x3m&mp_int=1&mp_locale=en_US&mp_player_type=IFRAME&mp_site_https_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_site_name=miniplay.com&mp_site_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_timezone=America%2FChicago&mp_view_type=&mini_signature=597f40034317bc8769082c7b72b9b1d0&xdm_e=https%3A%2F%2Fwww.miniplay.com&xdm_c=default7873&xdm_p=1&gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL3d3dy5taW5pcGxheS5jb20vZ2FtZS9tb3RvLXgzbSIsInBhcmVudERvbWFpbiI6Im1pbmlwbGF5LmNvbSIsInRvcERvbWFpbiI6Im1pbmlwbGF5LmNvbSIsImhhc0ltcHJlc3Npb24iOmZhbHNlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJob3N0IjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJ2ZXJzaW9uIjoiMS41LjE2In0%253D');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function ltf1() {
+  agU = Ultraviolet.codec.xor.encode('https://f.silvergames.com/ruffle/player.php?id=304');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function ltf2() {
+  agU = Ultraviolet.codec.xor.encode('https://f.silvergames.com/ruffle/player.php?id=1422');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function ltf3() {
+  agU = Ultraviolet.codec.xor.encode('https://f.silvergames.com/ruffle/player.php?id=5413');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function rooftopsnipers1() {
+  agU = Ultraviolet.codec.xor.encode('https://html5.gamedistribution.com/c3a70ae98547407a92ebedca8b79fdfa/?gd_sdk_referrer_url=https://2player.co/rooftop-snipers');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function rooftopsnipers2() {
+  agU = Ultraviolet.codec.xor.encode('https://ubg365.github.io/rooftop-snipers-2/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function vex3() {
+  agU = Ultraviolet.codec.xor.encode('https://html5.gamedistribution.com/762c932b4db74c6da0c1d101b2da8be6/?gd_sdk_referrer_url=https://vex-3.com/vex-3');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function vex4() {
+  agU = Ultraviolet.codec.xor.encode('https://html5.gamedistribution.com/80e6a5ae477f4d4fbcd1ea293d10087d/?gd_sdk_referrer_url=https://vex-3.com/vex-4');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function vex5() {
+  agU = Ultraviolet.codec.xor.encode('https://html5.gamedistribution.com/f120262ab72743039fbce88c1f370df8/?gd_sdk_referrer_url=https://vex-3.com/vex-5');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function vex6() {
+  agU = Ultraviolet.codec.xor.encode('https://html5.gamedistribution.com/4925e12574364121a48c61c35f649c36/?gd_sdk_referrer_url=https://vex-3.com/vex-6');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function worldshardestgame() {
+  agU = Ultraviolet.codec.xor.encode('https://watchdocumentaries.com/wp-content/uploads/games/worlds-hardest-game/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+function evio() {
+  agU = Ultraviolet.codec.xor.encode('https://ev.io/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
