73fcaa5fa0b394720096c88a14399f3136cdc7e4
Bʟ4ᴄᴋUɴ1x_Tᴇᴀᴍ <153548839+bl4ckun1x-team@users.noreply.github.com>
2024-04-09 17:44:26 +0200

    correct the thing

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use(
 if (config.challenge) {
   console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users))
   console.log('Passwords are: ' + Object.values(config.users))
-
   app.use(
     basicAuth({
       users: config.users,
