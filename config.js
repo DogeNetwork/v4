a3cbd4ed48e8390374e5e07b988ca2eb2f44b24d
Bʟ4ᴄᴋUɴ1x_Tᴇᴀᴍ <153548839+bl4ckun1x-team@users.noreply.github.com>
2024-04-09 17:24:47 +0200

    password protection DONE!

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  challenge: true, // Set to true if you want to enable password protection.
+  users: {
+    // You can add multiple users by doing username: 'password'.
+    interstellar: 'h4ck1ng_',
+  },
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: true, // Change this to false to disable local assets.
+}
+export default config
