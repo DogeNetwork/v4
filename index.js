dc0dee2081abcbdfd57854779df400405f0eafb7
FamnTax <152030665+FamnTax@users.noreply.github.com>
2024-04-11 21:15:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ server.on("upgrade", (req, socket, head) => {
 });
 
 server.on("listening", () => {
-  console.log(`Doge Unblocker has sucessfully started!\nListening on localhost (Port 8000).`);
+  console.log(`Skibidi owns contentkeeper LOL!\nListening on localhost (Port 8000).`);
 });
 
 server.listen({
