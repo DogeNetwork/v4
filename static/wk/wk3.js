cdfe1b4c910362719794043160dfc59ad31f0d3d
root <91960533+DerpmanDev@users.noreply.github.com>
2024-09-01 14:52:00 -0400

    Mistake

--- static/wk/wk3.js
+++ static/wk/wk3.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/sv/',
-    bare: 'https://surfdoge.pro/bear/',
+    bare: '/bear/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/wk/wk1.js', // handler
